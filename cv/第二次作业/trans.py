a48b021379a087fa97e34d2f07bd7759b45c2fdc
zzx <2030376534@qq.com>
2025-09-29 19:05:59 +0800

    修复透视变换错误，添加4个点用于透视变换

--- cv/第二次作业/trans.py
+++ cv/第二次作业/trans.py
@@ -18,8 +18,13 @@ affine_matrix = cv2.getAffineTransform(src_points, dst_points)
 affine_image = cv2.warpAffine(image, affine_matrix, (cols, rows))
 cv2.imwrite(output_file_1, affine_image)
 
-# 定义透视变换矩阵
+# 定义透视变换矩阵的源点和目标点
+src_points = np.float32([[0, 0], [cols - 1, 0], [cols - 1, rows - 1], [0, rows - 1]])
+dst_points = np.float32([[50, 50], [cols - 100, 50], [cols - 100, rows - 100], [50, rows - 100]])
+
+# 计算透视变换矩阵
 perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+
 # 应用透视变换
 perspective_image = cv2.warpPerspective(image, perspective_matrix, (cols, rows))
 cv2.imwrite(output_file_2, perspective_image)
