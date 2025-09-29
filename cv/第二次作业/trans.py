dc56ae1a48b72de2d94f040bcd99b9c78fdfc76e
zzx <2030376534@qq.com>
2025-09-29 18:48:26 +0800

    变换

---
+++ cv/第二次作业/trans.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+input_file = "1.png"
+output_file_1 = "affine.png"
+output_file_2 = "perspective.png"
+
+# 读取输入图像
+image = cv2.imread(input_file)
+
+# 定义仿射变换矩阵
+rows, cols, ch = image.shape
+src_points = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1]])
+dst_points = np.float32([[50, 50], [cols - 100, 50], [50, rows - 100]])
+affine_matrix = cv2.getAffineTransform(src_points, dst_points)
+
+# 应用仿射变换
+affine_image = cv2.warpAffine(image, affine_matrix, (cols, rows))
+cv2.imwrite(output_file_1, affine_image)
+
+# 定义透视变换矩阵
+perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+# 应用透视变换
+perspective_image = cv2.warpPerspective(image, perspective_matrix, (cols, rows))
+cv2.imwrite(output_file_2, perspective_image)
+
+
+
